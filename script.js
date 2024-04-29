81d372eae7ee6539a1eaca27a7b6f61a2808db81
Stephany Camily <stefycamly@gmail.com>
2024-04-29 13:47:27 -0300

    Projeto incompleto

---
+++ script.js
@@ -0,0 +1,18 @@
+function allowDrop(event){
+    event.preventDefault();
+}
+
+function drag(event){
+    event.dataTransfer.setData("text", event.target.id);
+}
+
+function drop(event){
+    event.preventDefault();
+    var data = event.dataTransfer.getData("text");
+    event.target.appendChild(document.getElementById(data));
+}
+
+function resetGame(){
+    document.querySelector(".reset").addEventListener("click", resetGame);
+    location.reload();
+}
